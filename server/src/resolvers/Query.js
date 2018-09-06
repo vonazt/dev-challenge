9e290534e5ec061067d2a77388e3d2a5413af4c0
Richard Tzanov <richardtzanov@gmail.com>
2018-09-06 15:35:26 +0100

    makes a query whenever a user selects product or price based on id

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -13,9 +13,19 @@ function allProducts(parent, args, context, info) {
 function product(parent, args, context, info) {
   return context.db.query.products({ where: { id: args.id } }, info);
 }
+function user(parent, args, context, info) {
+  return context.db.query.users({ where: { id: args.id } }, info);
+}
 
 function allUsers(parent, args, context, info) {
-  return context.db.query.users({}, info);
+  const where = args.filter
+    ? {
+      OR: [
+        { name_contains: args.filter }
+      ]
+    }
+    : {};
+  return context.db.query.users({ where }, info);
 }
 
 function userProducts(parent, args, context, info) {
@@ -26,5 +36,6 @@ module.exports = {
   allProducts,
   product,
   allUsers,
-  userProducts
+  userProducts,
+  user
 };
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import Product from './Product';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const PRODUCTS_QUERY = gql`
+const SUPPLIERS_QUERY = gql`
+{
+  allUsers {
+    name
+    id
+  }
+}
+`;
+
+const SINGLE_SUPPLIER_QUERY = gql`
+  query SingleSupplierQuery($id: ID!)
   {
-    allProducts {
-      name
-      id
-      price
-      suppliedBy {
+    user(id: $id) {
+      products {
         name
         id
       }
@@ -17,16 +23,11 @@ const PRODUCTS_QUERY = gql`
   }
 `;
 
-const SUPPLIERS_QUERY = gql`
+const PRODUCT_PRICE_QUERY = gql`
+  query ProductPriceQuery($id: ID!)
   {
-    allUsers {
-      name
-      id
-      products {
-        name
-        price
-        id
-      }
+    product(id: $id) {
+      price
     }
   }
 `;
@@ -40,68 +41,91 @@ class ProductList extends Component {
   }
 
   render() {
-    const { pickedSupplier, pickedProduct } = this.state;
+    const { pickedSupplier, pickedProduct, supplierId, productId } = this.state;
     return (
-      <Query query={SUPPLIERS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>;
-          if (error) return <div>Error</div>;
+      <div>
+        <Query query={SUPPLIERS_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error</div>;
 
-          const suppliersToRender = data.allUsers;
-          return (
-            <div>
-              <div className="field column is-half-mobile is-half-desktop is-half-tablet">
-                <label className="label">Supplier</label>
-                <div className="control">
-                  <div className="select is-fullwidth">
-                    <select
-                      value={pickedSupplier}
-                      onChange={(e) => {
-                        this.setState({ pickedSupplier: e.target.value });
-                      }}
-                    >
-                      <option disabled value="">
-                        Pick supplier
-                      </option>
-                      {suppliersToRender.map(supplier =>
-                        <option key={supplier.id} data-id={supplier.id}>{supplier.name}</option>
-                      )}
-                    </select>
-                    <div>{pickedSupplier && suppliersToRender.includes(pickedSupplier)}</div>
+            const suppliersToRender = data.allUsers;
+            return (
+              <div>
+                <div className="field column is-half-mobile is-half-desktop is-half-tablet">
+                  <label className="label">Supplier</label>
+                  <div className="control">
+                    <div className="select is-fullwidth">
+                      <select
+                        value={pickedSupplier}
+                        onChange={(e) => {
+                          const selectedIndex = e.target.options.selectedIndex;
+                          const supplierId = e.target.options[selectedIndex].getAttribute('data-id');
+                          this.setState({ pickedSupplier: e.target.value, supplierId: supplierId});
+                        }}
+                      >
+                        <option disabled value="">
+                          Pick supplier
+                        </option>
+                        {suppliersToRender.map(supplier =>
+                          <option key={supplier.id} data-id={supplier.id}>{supplier.name}</option>
+                        )}
+                      </select>
+                    </div>
                   </div>
                 </div>
               </div>
-              {this.state.pickedSupplier && <div className="field column is-half-mobile is-half-desktop is-half-tablet">
-                <label className="label">Product</label>
-                <div className="control">
-                  <div className="select is-fullwidth">
-                    <select
-                      value={pickedProduct}
-                      onChange={(e) => this.setState({ pickedProduct: e.target.value })}>
-                      <option disabled value="">
-                        Pick product
-                      </option>
-                      {suppliersToRender.map(supplier =>
-                        supplier.name === pickedSupplier && supplier.products.map(product =>
-                          <option key={product.id}>{product.name}</option>
-                        )
-                      )}
-                    </select>
+            );
+          }}
+        </Query>
+        {pickedSupplier && <Query query={SINGLE_SUPPLIER_QUERY} variables={{ id: supplierId }}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error</div>;
+
+            const productsToRender = data.user[0].products;
+            return (
+              <div>
+                <div className="field column is-half-mobile is-half-desktop is-half-tablet">
+                  <label className="label">Products</label>
+                  <div className="control">
+                    <div className="select is-fullwidth">
+                      <select
+                        value={pickedProduct}
+                        onChange={(e) => {
+                          const selectedIndex = e.target.options.selectedIndex;
+                          const productId = e.target.options[selectedIndex].getAttribute('data-id');
+                          this.setState({ pickedProduct: e.target.value, productId: productId });
+                        }}
+                      >
+                        <option disabled value="">
+                          Pick product
+                        </option>
+                        {productsToRender.map(product =>
+                          <option key={product.id} data-id={product.id}>{product.name}</option>
+                        )}
+                      </select>
+                    </div>
                   </div>
                 </div>
-              </div>}
-              {this.state.pickedProduct &&
-                <div>
-                  {suppliersToRender.map(supplier =>
-                    supplier.name === pickedSupplier && supplier.products.map(product =>
-                      product.name === pickedProduct && <div key={product.id}>{product.price}</div>
-                    )
-                  )}
-                </div>}
-            </div>
-          );
-        }}
-      </Query>
+              </div>
+            );
+          }}
+        </Query>}
+        {pickedProduct && <Query query={PRODUCT_PRICE_QUERY} variables={{ id: productId }}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error</div>;
+
+            const product = data.product[0];
+            return (
+              <div>
+                <p>It costs {product.price}</p>
+              </div>
+            );
+          }}
+        </Query>}
+      </div>
     );
   }
 }
